eed895e9ba014be7ce0d13b8e907813d56bfa169
Irina Romjalis <iromjalis@gmail.com>
2020-12-23 08:28:33 +0200

    final version

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $('.slaider').slick({
+       dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+      variableWidth: true,
+  arrows: false,
+   });
+});
